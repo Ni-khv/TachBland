2ef8c67bf9def00c828467961f08c7b52ecf686f
Ni-khv <ivasenetanikita@gmail.com>
2024-08-15 20:01:10 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HeaderPage from './components/HeaderPage/header_page';
+
+const App = () => {
+  return (
+    <div className="App">
+     <HeaderPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HeaderPage/header_page.jsx
@@ -0,0 +1,12 @@
+import './header_page.css'
+
+const HeaderPage = () => {
+
+    return(
+        <div className="header_block">
+        <div className="header_logo"></div>
+        </div>
+    )
+}
+
+export default HeaderPage;
